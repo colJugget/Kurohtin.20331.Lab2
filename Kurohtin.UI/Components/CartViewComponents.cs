5bef99be99d9f326d978b342f9600d5e4554cf61
Usser <Usser@USERPC>
2024-06-03 21:25:08 +0300

    Add project files.

---
+++ Kurohtin.UI/Components/CartViewComponents.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppLB1.Components
+{
+    public class CartViewComponents : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }
+    }
+}
